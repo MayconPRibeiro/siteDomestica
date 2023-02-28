54c05cbe8e8838771191e68996892f2b5c2ae8ae
MayconPRibeiro <104037787+MayconPRibeiro@users.noreply.github.com>
2023-02-28 19:20:04 -0300

    Update Conexao.php

--- PHP/domestica/Conexao.php
+++ PHP/domestica/Conexao.php
@@ -2,11 +2,15 @@
 
 namespace siteDomestica\PHP\domestica;
 
+
 class Conexao{
     
     public function conectar(){
         try{
-            $conn = mysqli_connect('localhost', 'root' , '' ,'site');
+            $conn = mysqli_init();
+            mysqli_ssl_set($conn,NULL,NULL, "{path to CA cert}", NULL, NULL);
+            mysqli_real_connect($conn, "faxilover-server.mysql.database.azure.com", "ipbeeawqyt", "Faxi@lover", "site", 3306, MYSQLI_CLIENT_SSL);
+
             if($conn){
                 
                 return $conn;
